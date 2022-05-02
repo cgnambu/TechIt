2d4dbf9ea70fd30dd13c3a4249aa06ae85c4dd24
Bryan <bryantsai75@gmail.com>
2022-05-02 15:13:07 -0700

    Merging work

--- techItTest/scripts/Projectile.cs
+++ techItTest/scripts/Projectile.cs
@@ -4,25 +4,49 @@ using System;
 public class Projectile : KinematicBody2D
 {
     [Export] float XSPEED = 0f;
-    [Export] float YSPEED = -100f;
-    Vector2 velocity = new Vector2(0f, 0f);
+    [Export] float YSPEED = 100f;
+    public int Speed = 200;
+    Vector2 velocity = new Vector2();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-
+        
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
+    public override void _PhysicsProcess(float delta)
     {
-        velocity.x = XSPEED;
-        velocity.y = YSPEED;
-        MoveAndSlide(velocity);
+        //velocity.x = XSPEED;
+        //velocity.y = YSPEED;
+        //MoveAndSlide(velocity);
+        var collision = MoveAndCollide(velocity * delta);
+        if (collision != null)
+        {
+            velocity = velocity.Bounce(collision.Normal);
+            if (collision.Collider.HasMethod("Hit"))
+            {
+                collision.Collider.Call("Hit");
+            }
+        }
+        pointProjectile();
+    }
+    public void Start(Vector2 pos, float dir)
+    {
+        Rotation = dir;
+        Position = pos;
+        velocity = new Vector2(0, Speed).Rotated(Rotation);
     }
 
     void delete()   //Deletes projectile, called from shield when intercepted
     {
         QueueFree();
+    }
+    
+    void pointProjectile()
+    {
+        var player = (KinematicBody2D)GetNode("/root/test/playerCore");
+        LookAt(player.GlobalPosition);  
+        RotationDegrees += 90;    
     }
 }
--- techItTest/scripts/cannon.cs
+++ techItTest/scripts/cannon.cs
@@ -1,17 +1,54 @@
 using Godot;
 using System;
 
-public class Cannon : KinematicBody2D
+public class cannon : KinematicBody2D
 {
+    PackedScene projectileScene;
+    public int Speed = 200;
+    private Vector2 _velocity = new Vector2();
+
+    
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        projectileScene = GD.Load<PackedScene>("res://scenes/projectile.tscn");
+        GetNode<Timer>("FireTimer").Start();
 
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
+    public override void _PhysicsProcess(float delta)
     {
-
+        pointCannon();          
+    }
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventKey eventKey) 
+        {
+            if (eventKey.Pressed && eventKey.Scancode == (int)KeyList.Space) 
+            {
+                fire();
+                GetTree().SetInputAsHandled();
+            }
+        }
+    }
+    void fire()
+    {
+        var projectile = (Projectile)projectileScene.Instance();
+        projectile.Start(GetNode<Node2D>("Barrel").GlobalPosition, Rotation);
+        GetParent().AddChild(projectile);
+    }
+    void pointCannon() 
+    {   
+        var player = (KinematicBody2D)GetNode("/root/test/playerCore");
+        LookAt(player.GlobalPosition);  
+        RotationDegrees += 90;
+    }
+    private void _on_FireTimer_timeout()
+    {
+        fire();
     }
 }
+
+
+    
